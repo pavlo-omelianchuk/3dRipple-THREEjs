e3b2b8a5610bcbf34734ebf68b0ec3ea30f521ee
Pavlo Omelianchuk <pavlotvingo@gmail.com>
2021-10-18 15:48:06 +0100

    Exploring react-three-fiber

--- src/App.js
+++ src/App.js
@@ -1,23 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import {
+  Canvas,
+  useLoader,
+  useFrame,
+  extend,
+  useThree,
+} from "react-three-fiber";
+import circleImg from "./assets/circle.png";
+import { Suspense, useCallback, useMemo, useRef } from "react";
+
+extend({ OrbitControls });
+
+function CameraControls() {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  const controlsRef = useRef();
+  useFrame(() => controlsRef.current.update());
+
+  return (
+    <orbitControls
+      ref={controlsRef}
+      args={[camera, domElement]}
+      autoRotate
+      autoRotateSpeed={-0.2}
+    />
+  );
+}
+
+function Points() {
+  const imgTex = useLoader(THREE.TextureLoader, circleImg);
+  const bufferRef = useRef();
+
+  // look for meaning of the following vars
+  // https://youtu.be/wRmeFtRkF-8?t=1041
+  //https://www.mathsisfun.com/algebra/amplitude-period-frequency-phase-shift.html
+  let t = 0,
+    f = 0.002,
+    a = 3;
+
+  const graph = useCallback(
+    (x, z) => {
+      return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
+    },
+    [t, f, a]
+  );
+
+  const count = 500;
+  const sep = 3;
+  let positions = useMemo(() => {
+    let positions = [];
+
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+        let y = graph(x, z);
+        positions.push(x, y, z);
+      }
+    }
+    return new Float32Array(positions);
+  }, [count, sep, graph]);
+
+  useFrame(() => {
+    t += 15;
+    const positions = bufferRef.current.array;
+
+    let i = 0;
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+
+        positions[i + 1] = graph(x, z);
+        i += 3;
+      }
+    }
+
+    bufferRef.current.needsUpdate = true;
+  });
+
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          ref={bufferRef}
+          attachObject={["attributes", "position"]}
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <pointsMaterial
+        attach="material"
+        map={imgTex}
+        color={0xfface4}
+        size={0.2}
+        sizeAttenuation
+        transparent={false}
+        alphaTest={0.5}
+        opacity={1.0}
+      />
+    </points>
+  );
+}
+
+function AnimationCanvas() {
+  return (
+    <Canvas
+      colorManagment={false}
+      camera={{ position: [100, 120, 50], fov: 40 }}
+    >
+      <Suspense fallback={null}>
+        <Points />
+      </Suspense>
+      <CameraControls />
+    </Canvas>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="animWrapepr">
+      <a href="/">
+        <span className="cta-btn"> Main Text</span>
+      </a>
+
+      <div className="anim">
+        <Suspense fallback={<div>Loading...</div>}>
+          <AnimationCanvas />
+        </Suspense>
+      </div>
     </div>
   );
 }
